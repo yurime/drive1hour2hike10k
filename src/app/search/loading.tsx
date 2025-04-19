a4fd28bb749e7676cbbab48b9a7a19d32aa569e9
Yuri Meshman <syurim@gmail.com>
2025-04-19 19:42:03 +0300

    Stylistic changes
    - "Blog" replaced with "Hiking Blog"
    - Loading file given more structure although it might not be used yet (may be used if will switch to DB)

--- src/app/search/loading.tsx
+++ src/app/search/loading.tsx
@@ -1,3 +1,9 @@
+
+import Container from '../_components/container';
+
 export default function Loading() {
-  return <div>Loading...</div>;
+  return (
+    <Container>
+      <div>Loading...</div>
+    </Container>);
 }

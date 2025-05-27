9263a9cf8b81aff15d8596ce2be24d75a37c9d8a
Yuri Meshman <syurim@gmail.com>
2025-05-27 13:58:19 +0300

    cleanup
    - Adding some end file line breaks
    - removing some unused files
    - removing some unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { HeroPost } from "@/app/_components/hero-post";
 import { Intro } from "@/app/_components/intro";
 import { MoreStories } from "@/app/_components/more-stories";
 import { getAllPosts } from "@/lib/api";
-import Link from "next/link";
 
 export default function Index() {
   const allPosts = getAllPosts();

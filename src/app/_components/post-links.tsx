b3e52f80809441f0553ecb4f13d3dbbdf6136c73
Yuri Meshman <syurim@gmail.com>
2025-05-29 14:40:27 +0300

    adding amudanan route button that should work on vercel server

--- src/app/_components/post-links.tsx
+++ src/app/_components/post-links.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 
 export function PostLinks(
   {gpxFileAddr, wazeParking, israelHikingMap}:{gpxFileAddr:string, wazeParking:string, israelHikingMap:string}) {
-  const basePath = process.env.APIpath;
   return (
     <>
       <Link 
@@ -26,13 +25,13 @@ export function PostLinks(
              Route on IsraelHikingMap 
            </button>
       </Link>
-             {basePath && <Link 
-                       href={'https://amudanan.co.il/?url=' + process.env.APIpath + gpxFileAddr}
+              <Link 
+                       href={'https://amudanan.co.il/?url='  + gpxFileAddr}
                        className="hover:underline">
                         <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
                           Route on Amudanan 
                         </button>
-                   </Link>}
+                   </Link>
     </>
   );
 };
